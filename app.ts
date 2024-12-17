9633a8666a55483e144f8a2c5c806a2abb6b27c6
Nuttawut Malee <nuttawutmalee@gmail.com>
2024-12-17 15:01:00 +0700

    Make postgres db into singleton class

--- app.ts
+++ app.ts
@@ -3,22 +3,12 @@
 import 'dotenv/config';
 import express from 'express';
 
-// import { connectDB } from '@app/db/postgres.db';
-
-// const db = connectDB({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   name: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   pass: process.env.DB_PASS,
-// });
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, world!' });
+app.get('/', (_req, res) => {
+    res.status(200).json({ message: 'Hello, world!' });
 });
 
 app.listen(3000, () => {
-  console.log(`server running on port 3000`);
+    console.log(`server running on port 3000`);
 });
--- app/db/postgres.db.ts
+++ app/db/postgres.db.ts
@@ -2,22 +2,43 @@
 
 import pgPromise from 'pg-promise';
 
-const pgp = pgPromise();
+// PostgresDB represents singleton class of postgres database
+class PostgresDB {
+    private static _instance: PostgresDB;
+    private readonly _pgp: pgPromise.IMain;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private readonly _db: pgPromise.IDatabase<any>;
 
-export const connectDB = ({
-  host = 'localhost',
-  port = '5432',
-  name,
-  user,
-  pass,
-}: {
-  host: string | undefined;
-  port: string | undefined;
-  name: string | undefined;
-  user: string | undefined;
-  pass: string | undefined;
-}): pgPromise.IDatabase<object> => {
-  return pgp(
-    `postgres://${user}:${pass}@${host}:${port}/${name}?sslmode=disable`,
-  );
-};
+    private constructor() {
+        this._pgp = pgPromise();
+
+        const {
+            DB_HOST = 'localhost',
+            DB_PORT = '5432',
+            DB_USER,
+            DB_PASS,
+            DB_NAME,
+        } = process.env;
+
+        this._db = this._pgp(
+            `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable`,
+        );
+    }
+
+    static getInstance() {
+        if (!this._instance) {
+            this._instance = new PostgresDB();
+        }
+        return this._instance;
+    }
+
+    public get pgp() {
+        return this._pgp;
+    }
+
+    public get db() {
+        return this._db;
+    }
+}
+
+export default PostgresDB;
