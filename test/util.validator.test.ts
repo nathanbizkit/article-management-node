7079720f8099c3a279a484d176b526651f7642e3
Nuttawut Malee <nuttawutmalee@gmail.com>
2025-03-04 13:16:22 +0700

    Refactor

--- test/util.validator.test.ts
+++ test/util.validator.test.ts
@@ -5,35 +5,37 @@ import { buildValidationMessage } from '#app/util/validator.js';
 import { expect } from 'chai';
 
 describe('util validator', function () {
-    it('should build validation message', function () {
-        const expected = 'validation error: foo, bar';
+    describe('build validation message', function () {
+        it('should build validation message', function () {
+            const expected = 'validation error: foo, bar';
 
-        const validationErrorItems = [
-            { message: 'foo', path: [], type: 'foo' },
-            { message: 'bar', path: [], type: 'bar' },
-        ];
-        const validationError = new joi.ValidationError(
-            'validation error',
-            validationErrorItems,
-            null,
-        );
-        const validationMessage = buildValidationMessage(validationError);
-        expect(validationMessage).to.equal(expected);
-    });
+            const validationErrorItems: joi.ValidationErrorItem[] = [
+                { message: 'foo', path: [], type: 'foo' },
+                { message: 'bar', path: [], type: 'bar' },
+            ];
+            const validationError = new joi.ValidationError(
+                'validation error',
+                validationErrorItems,
+                null,
+            );
+            const validationMessage = buildValidationMessage(validationError);
+            expect(validationMessage).to.equal(expected);
+        });
 
-    it('should build validation message without parent message', function () {
-        const expected = 'foo, bar';
+        it('should build validation message without parent message', function () {
+            const expected = 'foo, bar';
 
-        const validationErrorItems = [
-            { message: 'foo', path: [], type: 'foo' },
-            { message: 'bar', path: [], type: 'bar' },
-        ];
-        const validationError = new joi.ValidationError(
-            '',
-            validationErrorItems,
-            null,
-        );
-        const validationMessage = buildValidationMessage(validationError);
-        expect(validationMessage).to.equal(expected);
+            const validationErrorItems: joi.ValidationErrorItem[] = [
+                { message: 'foo', path: [], type: 'foo' },
+                { message: 'bar', path: [], type: 'bar' },
+            ];
+            const validationError = new joi.ValidationError(
+                '',
+                validationErrorItems,
+                null,
+            );
+            const validationMessage = buildValidationMessage(validationError);
+            expect(validationMessage).to.equal(expected);
+        });
     });
 });
